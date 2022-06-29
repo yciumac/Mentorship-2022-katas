127ee325f8241d7ba501e8f8bdcd48ba016b528b
YuliaC <yciumac@gmail.com>
2022-06-29 22:18:18 +0300

    solved 02-isStringUppercase.js, added isStringUppercase.test.js

--- 02-isStringUppercase/02-isStringUppercase.js
+++ 02-isStringUppercase/02-isStringUppercase.js
@@ -1,5 +1,14 @@
 
-function isStringUppercase(string){
+function isStringUppercase(string) {
+   let format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+   if (format.test(string)) {
+      return `Invalid input`;
+   } else if (string === string.toUpperCase()) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
 
 module.exports = isStringUppercase;
---
+++ 02-isStringUppercase/isStringUppercase.test.js
@@ -0,0 +1,18 @@
+const isStringUppercase = require('./02-isStringUppercase');
+
+test('should return true when string isUppercase', () => {
+   var result = isStringUppercase('HELLO');
+   expect(result).toBe(true);
+});
+
+test('should return false when strig is not isUppercase', () => {
+   var result = isStringUppercase('Hello');
+   expect(result).toBe(false);
+});
+
+test('should return "Invalid input" when string contains unacceptable characters', () => {
+   var result = isStringUppercase('HELLO!!!!');
+   expect(result).toBe("Invalid input");
+})
+
+
